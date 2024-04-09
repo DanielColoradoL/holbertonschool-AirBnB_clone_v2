2fc92412061c0be2d364b527d5cf9921b85dac97
Daniel Colorado <danielcoloradol@hotmail.com>
2024-04-09 18:01:28 +1000

    feat(task 4):Number template

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python3
+""" tarts a Flask web application """
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def index():
+    """
+    Prints a message in the main page
+    """
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """
+    Prints a message in the /hbnb page
+    """
+    return "HBNB!"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def c_text(text):
+    """
+    display "C " followed by the value of the text
+    """
+    clean_text = text.replace("_", " ")
+    return "C " + clean_text
+
+
+@app.route("/python/")
+@app.route("/python/<text>", strict_slashes=False)
+def python_text(text="is cool"):
+    """
+    display "Python " followed by the value of the text
+    """
+    clean_text = text.replace("_", " ")
+    return "Python " + clean_text
+
+
+@app.route("/number/<int:n>", strict_slashes=False)
+def num_n(n):
+    """
+    display "num " followed by the value of the text
+    """
+    if isinstance(n, int):
+        return f"{n} is a number"
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
